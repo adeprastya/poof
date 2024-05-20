e3374a222a1456d4fbd51cbcca444da73b9aaee6
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2024-05-20 15:31:38 +0700

    add: Trash

---
+++ src/controllers/TrashController copy.php
@@ -0,0 +1,59 @@
+<?php
+include_once ('../models/Note.php');
+
+class TrashController
+{
+  public function new_note()
+  {
+    if (isset($_POST['new_note'])) {
+      $title = $_POST['title'];
+      $content = $_POST['content'];
+      $user_id = $_POST['user_id'];
+
+      $note = Note::create($title, $content, $user_id);
+
+      if ($note) {
+        header('Location: ../views/home.php?success=created');
+      } else {
+        header('Location: ../views/home.php?error=failed');
+      }
+    }
+  }
+
+  public function update_note()
+  {
+    if (isset($_GET['update_note'])) {
+      $title = $_POST['title'];
+      $content = $_POST['content'];
+      $id = $_GET['update_note'];
+
+      $note = Note::update($title, $content, $id);
+
+      if ($note) {
+        header('Location: ../views/home.php?success=updated');
+      } else {
+        header('Location: ../views/home.php?error=failed');
+      }
+    }
+  }
+
+  public function delete_note()
+  {
+    if (isset($_GET['delete_note'])) {
+      $id = $_GET['delete_note'];
+
+      $note = Note::delete($id);
+
+      if ($note) {
+        header('Location: ../views/home.php?success=deleted');
+      } else {
+        header('Location: ../views/home.php?error=failed');
+      }
+    }
+  }
+}
+
+$noteController = new NoteController();
+$noteController->new_note();
+$noteController->update_note();
+$noteController->delete_note();
---
+++ src/models/Trash.php
@@ -0,0 +1,49 @@
+<?php
+
+class Trash
+{
+  public static function getAll($user_id)
+  {
+    include ('../config/db.php');
+
+    $stmt = mysqli_prepare($conn, "SELECT * FROM note WHERE user_id = ?");
+    mysqli_stmt_bind_param($stmt, "i", $user_id);
+    mysqli_stmt_execute($stmt);
+
+    return mysqli_stmt_get_result($stmt)->fetch_all(MYSQLI_ASSOC);
+  }
+
+  public static function create($title, $content, $user_id)
+  {
+    include_once ('../config/db.php');
+
+    $stmt = mysqli_prepare($conn, "INSERT INTO note (title, content, user_id) VALUES (?, ?, ?)");
+    mysqli_stmt_bind_param($stmt, "ssi", $title, $content, $user_id);
+    mysqli_stmt_execute($stmt);
+
+    return mysqli_stmt_affected_rows($stmt);
+  }
+
+  public static function update($title, $content, $id)
+  {
+    include_once ('../config/db.php');
+
+    $stmt = mysqli_prepare($conn, "UPDATE note SET title = ?, content = ? WHERE id = ?");
+    mysqli_stmt_bind_param($stmt, "ssi", $title, $content, $id);
+    mysqli_stmt_execute($stmt);
+
+    return mysqli_stmt_affected_rows($stmt);
+  }
+
+  public static function delete($id)
+  {
+    include_once ('../config/db.php');
+
+    $stmt = mysqli_prepare($conn, "DELETE FROM note WHERE id = ?");
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+
+    return mysqli_stmt_affected_rows($stmt);
+  }
+
+}
