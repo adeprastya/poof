ddef2f21bfd4f2cbc9d6f811618ae4c201b826c0
reyhanfisena <115385745+reyhanfisena@users.noreply.github.com>
2024-06-07 20:32:15 +0700

    add:remindercontroller

---
+++ src/controllers/ReminderController.php
@@ -0,0 +1,69 @@
+<?php
+include_once ('../models/Note.php');
+
+class ReminderController 
+{
+  public function new_reminder()
+  {
+    if (isset($_POST['new_reminder'])) {
+      $note_id = $_POST['note_id'];
+      $remind_at = $_POST['remind_at'];
+
+      $reminder = Reminder::create($note_id, $remind_at);
+
+      if ($reminder) {
+        header('Location: ../views/reminders.php?success=created');
+      } else {
+        header('Location: ../views/reminders.php?error=failed');
+      }
+    }
+  }
+
+  public function update_reminder()
+  {
+    if (isset($_POST['update_reminder'])) {
+      $id = $_POST['update_reminder'];
+      $note_id = $_POST['note_id'];
+      $remind_at = $_POST['remind_at'];
+
+      $reminder = Reminder::update($id, $note_id, $remind_at);
+
+      if ($reminder) {
+        header('Location: ../views/reminders.php?success=updated');
+      } else {
+        header('Location: ../views/reminders.php?error=failed');
+      }
+    }
+  }
+
+  public function delete_reminder()
+  {
+    if (isset($_GET['delete_reminder'])) {
+      $id = $_GET['delete_reminder'];
+
+      $delete = Reminder::delete($id);
+      if ($delete) {
+        header('Location: ../views/reminders.php?success=deleted');
+      } else {
+        header('Location: ../views/reminders.php?error=failed');
+      }
+    }
+  }
+
+  public function send_reminders()
+  {
+    $reminders = Reminder::where('remind_at', '<=', now())->where('is_sent', 0)->get();
+    foreach ($reminders as $reminder) {
+      // Send reminder notification to user
+      // ...
+      $reminder->is_sent = 1;
+      $reminder->save();
+    }
+  }
+}
+
+$reminderController = new ReminderController();
+$reminderController->new_reminder();
+$reminderController->update_reminder();
+$reminderController->delete_reminder();
+$reminderController->send_reminders();
