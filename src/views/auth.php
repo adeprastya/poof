aada6497a01e1c600eea91b3118bbbf40ce6ea74
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2024-06-07 23:36:30 +0700

    feat: new font

--- src/views/auth.php
+++ src/views/auth.php
@@ -9,6 +9,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Poof | Login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200..800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/auth.css?v=<?= time() ?>">
 
     <script src="https://cdn.jsdelivr.net/npm/kursor@0.0.14/dist/kursor.js"></script>
